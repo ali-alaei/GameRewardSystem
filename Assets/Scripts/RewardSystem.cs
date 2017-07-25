c10e2287f764c408e8503bd67d3e4d90cfdf9063
ali <aainsrm@gmail.com>
2017-07-25 18:54:43 +0430

    system is completed with primary algorithm
    Bugs still Exist , I should try my new algorithm next

--- Assets/Scripts/RewardSystem.cs
+++ Assets/Scripts/RewardSystem.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 using System;
 
 
-public class RewardSystem : MonoBehaviour {
+public class RewardSystem : MonoBehaviour
+{
     [System.Serializable]
     private struct Prize
     {
@@ -39,6 +40,7 @@ public class RewardSystem : MonoBehaviour {
             }
         }
     }
+ 
     enum ProgressStates
     {
         Progressable,
@@ -46,41 +48,147 @@ public class RewardSystem : MonoBehaviour {
         NoProgress
     };
 
-    [SerializeField]
-    List<Prize> userPrizes;
-
-    
-    [SerializeField]
-    private int user_id;
-
-   
-    
-    private DateTime time1;
-    private DateTime time2;
-
-    
-    private void PlayerPrefsInit()
+    private enum TimeType
     {
-        time1 = new DateTime(2017, 7, 17, 14, 59, 05);
-        PlayerPrefs.SetString("LastRewarded:10", time1.ToString());
-        time2 = new DateTime(2017, 6, 24, 18, 30, 45);
-        PlayerPrefs.SetString("LastRewarded:11", time2.ToString());
-        PlayerPrefs.SetInt("Progress:10", 1);
-        PlayerPrefs.SetInt("Progress:11", 3);
-        
+        min,
+        hour,
+        day,
 
     }
 
-    private ProgressStates GetProgressState(int id)
+
+    [SerializeField]
+    private TimeType timeKind;
+
+    [SerializeField]
+    private Time favouriteTimes;
+
+    [SerializeField]
+    private List<Prize> userPrizes;
+
+
+    [SerializeField]
+    private int user_id;
+
+
+
+
+    [SerializeField]
+    [Range(0, 59)]
+    private int favourite_Minute_Difference;
+    [SerializeField]
+    [Range(0, 23)]
+    private int favourite_Hour_Difference;
+
+
+
+    private string sid;
+    private long lastRewardedTime;
+    private int userProgress;
+
+    void Start()
     {
-        string sid = id.ToString();
-        DateTime lastRewarded = Convert.ToDateTime(PlayerPrefs.GetString("LastRewarded:" + sid));
-        int difference = DateTime.Now.DayOfYear - lastRewarded.DayOfYear;
-        if (difference == 1 )
+        //PlayerPrefs.DeleteAll();
+        LoadFromDB();
+        GetReward();
+        //GetProgress(user_id);
+    }
+
+    private void LoadFromDB()
+    {
+        sid = user_id.ToString();
+
+        if (CheckPlayerExistence())
         {
-            return ProgressStates.Progressable;
+
+            lastRewardedTime = long.Parse(PlayerPrefs.GetString("LastRewarded:" + sid));
+            userProgress = PlayerPrefs.GetInt("Progress:" + user_id);
         }
-        else if(difference > 1)
+        else
+        {
+            DateTime time1 = new DateTime(2010, 7, 17, 14, 30, 05);
+            lastRewardedTime = time1.Ticks;
+            userProgress = 0;
+            SaveToDB();
+        }
+
+    }
+
+
+    private bool CheckPlayerExistence()
+    {
+        if (PlayerPrefs.HasKey("LastRewarded:" + user_id))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+
+    }
+
+    private ProgressStates GetProgressState()
+    {
+        //____checking with days____//
+
+        print("rewarded time format : " + lastRewardedTime);
+        TimeSpan lastRewarded = new TimeSpan(lastRewardedTime);
+        TimeSpan timeOfNow = new TimeSpan(DateTime.Now.Ticks);
+
+        print("last: " + lastRewarded.Minutes);
+
+        print("now:" + timeOfNow.Minutes);
+
+
+        double minutesDifference = timeOfNow.Minutes - lastRewarded.Minutes;
+        double hrsDiff = timeOfNow.Hours - lastRewarded.Hours;
+        double totalMin = timeOfNow.TotalMinutes - lastRewarded.TotalMinutes;
+        double totalHrs = timeOfNow.TotalHours - lastRewarded.TotalHours;
+        //____checking with minutes____//
+
+        switch (timeKind)
+        {
+            case TimeType.hour:
+
+                if (totalHrs<=favourite_Hour_Difference)
+                {
+                    if (hrsDiff == favourite_Hour_Difference)
+                    {
+                        return ProgressStates.Progressable;
+
+                    }
+                    else
+                    {
+                        return ProgressStates.NoProgress;
+
+                    }
+                }
+                else
+                {
+                    return ProgressStates.ResetProgress;
+
+                }
+                break;
+            case TimeType.min:
+                if (minutesDifference == favourite_Minute_Difference)
+                {
+
+                    if (totalMin <=  59)
+                    {
+                        return ProgressStates.Progressable;
+                    }
+
+                }
+                break;
+           
+        }
+
+
+
+
+        if (minutesDifference > favourite_Minute_Difference)
         {
             return ProgressStates.ResetProgress;
         }
@@ -88,93 +196,59 @@ public class RewardSystem : MonoBehaviour {
         {
             return ProgressStates.NoProgress;
         }
-    }
-
-    public void GetProgress(int uId)
-    {
-        if (GetProgressState(uId) == ProgressStates.Progressable)
-        {
-            PlayerPrefs.SetInt("Progress:" + uId, PlayerPrefs.GetInt("Progress:" + uId) + 1);
-        }
-        else if (GetProgressState(uId) == ProgressStates.ResetProgress)
-        {
-            PlayerPrefs.SetInt("Progress:" + uId, 0);
-        }
-        
-    }
-
-    private bool CanGetReward(int uId)
-    {
-        string sid = uId.ToString();
-        DateTime lastRewarded = Convert.ToDateTime(PlayerPrefs.GetString("LastRewarded:"+sid));
-        int difference = DateTime.Now.DayOfYear - lastRewarded.DayOfYear;
-        //print(difference);
-        if (difference>=1)
-        {
-            //print("it works");
-            return true;
-        }
-        else
-        {
-            //print("wrong answer");
-            return false;
-        }
 
     }
 
-    public void GetReward(int uId)
-    {       
-        //userPrize.setCoin();
-        if (CanGetReward(uId))
-        {
-            //int rand = UnityEngine.Random.Range(0, 10);
-            //print("rand = " + rand);
-            //if ()
-            //{
-            //PlayerPrefs.SetInt("UserReward:" + uId, userPrize.Coin);
-            //print(userPrize.Coin + " Coin Was Awrarded to user with userID : " + uId);
 
-            //}
-            //else if()
-            //{
-            //PlayerPrefs.SetInt("UserReward:" + uId, userPrize.Gem);
-            //print(userPrize.Gem + " Gem Was Awrarded to user with userID : " + uId);
-            //}
-            int progressState;
-            progressState = PlayerPrefs.GetInt("Progress:" + uId);
-            if (progressState<=userPrizes.Count)
-            {
-                print(userPrizes[--progressState].Coin + " coins were awarded to user with user id = " + uId);
-                print(userPrizes[--progressState].Gem + " gems were awarded to user with user id = " + uId);
 
-            }
-            else
-            {
-                print("you didn't assign enough prize");
-            }
 
-        }
-    }
-    
-    public void RewardDecleration()
+
+    public void GetReward()
     {
 
 
 
+        ProgressStates state = GetProgressState();
+
+        switch (state)
+        {
+            case ProgressStates.Progressable:
+                userProgress++;
+                GiveReward();
+                break;
+            case ProgressStates.ResetProgress:
+                userProgress = 1;
+                GiveReward();
+                break;
+            case ProgressStates.NoProgress:
+                break;
+            default:
+                break;
+        }
+
+        SaveToDB();
     }
-    
-    // Use this for initialization
-	void Start () {
-        PlayerPrefsInit();
-        GetReward(user_id);       
-        //Debug.Log(PlayerPrefs.GetString("LastRewarded:10"));
-        //Debug.Log(PlayerPrefs.GetString("LastRewarded:11"));
 
-        //print(Convert.ToDateTime(PlayerPrefs.GetString("LastRewarded:10")));
-        //int lastDay = time1.DayOfYear;
 
+
+    private void GiveReward()
+    {
+        print(userPrizes[userProgress - 1].Coin + " coins were awarded to user with user id = " + user_id);
+        print(userPrizes[userProgress - 1].Gem + " gems were awarded to user with user id = " + user_id);
+
+        lastRewardedTime = DateTime.Now.Ticks;
 
     }
+
+    private void SaveToDB()
+    {
+        PlayerPrefs.SetInt("Progress:" + user_id, userProgress);
+
+        PlayerPrefs.SetString("LastRewarded:" + user_id, lastRewardedTime.ToString());
+    }
+
+
+
 
 
 }
